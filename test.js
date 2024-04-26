29df02991eb03dea34568b147c98fe9eef05c654
aksh0000 <ruthlessdestroyer085@gmail.com>
2024-04-26 08:38:55 -0700

    version3

--- test.js
+++ test.js
@@ -7,7 +7,13 @@ app.get('/',(req,res)=>{
     res.send("working just fine!")
 })
 
-app.get('/class:choosenclass/subject:choosensubject/contenttype:contenttype/choosenfile:choosenfile',(req,res)=>{
+app.get('/trial/:name',(req,res)=>{
+    const filepath=path.resolve(__dirname,'folder','10','maths','boardpapers','maths2013.pdf');
+    console.log(req.params.name)
+    res.sendFile(filepath)
+})
+
+app.get('/class/:choosenclass/:choosensubject/:contenttype/:choosenfile',(req,res)=>{
     const filepath=path.resolve(__dirname,'folder',req.params.choosenclass,req.params.choosensubject,req.params.contenttype,req.params.choosenfile+".pdf");
     res.sendFile(filepath);
 });
