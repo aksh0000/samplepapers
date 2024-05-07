2c00020f38ea738ce99c3a9bcf4a291b095292ad
aksh0000 <ruthlessdestroyer085@gmail.com>
2024-05-07 06:37:31 -0700

    version3.1

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express=require('express');
 const app=express();
 const cors=require('cors');
 const path = require('path');
-app.use(cors());
+
 app.get('/',(req,res)=>{
-    res.send("working just fine!")
+    res.send("working just fine! now it is!!")
 })
 
-app.get('/class:choosenclass/subject:choosensubject/contenttype:contenttype/choosenfile:choosenfile',(req,res)=>{
+app.get('/trial/:name',(req,res)=>{
+    const filepath=path.resolve(__dirname,'folder','10','maths','boardpapers','maths2013.pdf');
+    console.log(req.params.name)
+    res.sendFile(filepath)
+})
+
+app.get('/class/:choosenclass/:choosensubject/:contenttype/:choosenfile',(req,res)=>{
     const filepath=path.resolve(__dirname,'folder',req.params.choosenclass,req.params.choosensubject,req.params.contenttype,req.params.choosenfile+".pdf");
     res.sendFile(filepath);
 });
